1125ec3670ea3a6fa1558313cb0458b21374458a
justinakoh <justina.y.koh@gmail.com>
2020-11-08 17:28:24 +1300

    Update code to complete exlixir tutorial S5 p41
    Uses pattern matching and structs to get the RGB value that we will use on the identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,6 +9,20 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_colour
+  end
+
+  @doc """
+    This gets the first three values from the hexlist to get a RGB colour.
+    This pattern matching on the struct to get the first three values
+  """
+  def pick_colour(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    #This gets the first three values, and acknowledges the rest of the values
+    #but we don't actualyl care about it. It is the same as the commented code below
+    #[r, g, b | _tail] = hex_list
+
+    [r, g, b]
   end
 
   @doc """
