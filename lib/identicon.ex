638922f30cd140ec628f047bb2e1a6ab2db26283
justinakoh <justina.y.koh@gmail.com>
2020-11-08 16:57:34 +1300

    Update code to complete exlixir tutorial S5 p37
    This creates two new methods, main and another method which hashes the input

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,18 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for `Identicon`.
+    `Identicon` is an app which receives a string and creates an identicon based off of the string.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+    Main. Pretty basic stuff
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
