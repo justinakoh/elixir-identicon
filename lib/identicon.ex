3414b5021485ecb69600285a8d6701ca22adfd8f
justinakoh <justina.y.koh@gmail.com>
2020-11-08 18:00:20 +1300

    Update code to complete exlixir tutorial S5 p44
    Create new method for mirroring the rows and compelte it properly

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -16,7 +16,7 @@ defmodule Identicon do
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
     |> Enum.chunk(3)
-    |> mirror_row
+    |> Enum.map(&mirror_row/1) #stuffs every row into a new list map thing
   end
 
   def mirror_row(row) do
